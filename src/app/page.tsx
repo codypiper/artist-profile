3fbaef44458ef5d9298f49a79f163ac9a4398217
Cody Piper <piperccody@gmail.com>
2023-10-15 04:20:25 -0400

    Refactor components to use arrow functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>Cody Piper</h1>
-    </main>
-  );
-}
+const Home = () => (
+  <main className="flex min-h-screen flex-col items-center justify-between">
+    <h1>Cody Piper</h1>
+  </main>
+);
+
+export default Home;
