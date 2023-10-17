7cf4e603cf84055e892702cdbc719d092eb01094
Cody Piper <piperccody@gmail.com>
2023-10-17 17:54:29 -0400

    Generate a manifest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+const manifest = (): MetadataRoute.Manifest => ({
+  name: "Cody Piper",
+  short_name: "Cody Piper",
+  description: "Official website of singer/songwriter Cody Piper",
+  start_url: "/",
+  display: "standalone",
+  background_color: "black",
+  icons: [
+    {
+      src: "/icon.png",
+      sizes: "128x128",
+      type: "image/png",
+    },
+  ],
+  categories: ["entertainment", "music"],
+});
+
+export default manifest;
