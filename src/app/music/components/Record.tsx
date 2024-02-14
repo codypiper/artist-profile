1667639daac398c17b24cac10f000e00d7fdb139
Cody Piper <piperccody@gmail.com>
2024-02-13 21:41:33 -0500

    Make Record component shrink on smaller viewports

--- src/app/music/components/Record.tsx
+++ src/app/music/components/Record.tsx
@@ -6,9 +6,9 @@ type RecordProps = IRecord;
 
 const Record = ({ title, href, image, type, date }: RecordProps) => (
   <ExternalLink href={href}>
-    <div className="flex flex-col items-center">
+    <div className="flex w-64 max-w-[35vw] flex-col items-center">
       <Image
-        className="mb-2"
+        className="mb-2 w-full"
         src={image}
         alt={`${title} cover artwork`}
         priority
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
 };
 
 const Music = () => (
-  <main className="relative flex min-h-screen flex-col items-center justify-center px-4 py-40">
+  <main className="relative flex min-h-screen flex-col items-center justify-center px-6 py-40">
     <h1 className="mb-10 text-center text-3xl font-bold drop-shadow-dark">
       Music
     </h1>
-    <ol className="flex flex-wrap justify-center gap-6">
+    <ol className="flex flex-wrap justify-center gap-8">
       {records.map((record) => (
         <li key={record.title}>
           <Record {...record} />
