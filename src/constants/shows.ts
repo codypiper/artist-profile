3bd335caeaf71355a07e82327a7274ca223fc642
Cody Piper <piperccody@gmail.com>
2024-03-05 20:32:15 -0500

    Add list of shows to reference for Shows page

---
+++ src/constants/shows.ts
@@ -0,0 +1,34 @@
+import Show from "@/types/Show";
+
+const shows: Show[] = [
+  {
+    name: "Songwriters in the Cellar",
+    venue: "Pittsburgh Winery",
+    city: "Pittsburgh, PA",
+    date: new Date("2024-03-02T20:00"),
+    href: "https://bit.ly/GamblePiperHallowayKoulourisShafer",
+  },
+  {
+    name: "Rounds at the Woods",
+    venue: "Woods House",
+    city: "Pittsburgh, PA",
+    date: new Date("2024-04-04T19:00"),
+    href: "https://www.bandsintown.com/e/105261838",
+  },
+  {
+    name: "w/ Julia Norah",
+    venue: "Jenks1929",
+    city: "Akron, OH",
+    date: new Date("2024-05-11T16:00"),
+    href: "https://www.bandsintown.com/e/105262835",
+  },
+  {
+    name: "McCandless Crossing Summer Concert Series",
+    venue: "McCandless Crossing",
+    city: "Pittsburgh, PA",
+    date: new Date("2024-08-01T18:00"),
+    href: "https://www.bandsintown.com/e/105262829",
+  },
+];
+
+export default shows;
---
+++ src/types/Show.ts
@@ -0,0 +1,7 @@
+export default interface Show {
+  name: string;
+  venue: string;
+  city: string;
+  date: Date;
+  href: string;
+}
