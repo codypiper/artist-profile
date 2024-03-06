650984ce596ff4fdb97cbb0e46bb084922e5b696
Cody Piper <piperccody@gmail.com>
2024-03-06 00:31:16 -0500

    Define ButtonLinkProps type for readability

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,11 +1,13 @@
 import Button from "./Button";
 import Link from "./Link";
 
+type ButtonLinkProps = React.ComponentProps<typeof Link>;
+
 const ButtonLink = ({
   children,
   noShadow = false,
   ...props
-}: React.ComponentProps<typeof Link>) => (
+}: ButtonLinkProps) => (
   <Link {...props} noShadow>
     <Button noShadow={noShadow}>{children}</Button>
   </Link>
