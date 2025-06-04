a3986b3120da63d8defb01c1f0e3413ba79402a5
Cody Piper <piperccody@gmail.com>
2025-06-04 01:37:45 -0400

    Add lightbox for photos

--- src/app/photos/components/PhotoGallery.tsx
+++ src/app/photos/components/PhotoGallery.tsx
@@ -2,19 +2,22 @@
 
 import photos from "@/data/photos";
 import Image from "next/image";
+import { useState } from "react";
 import {
   RenderImageContext,
   RenderImageProps,
   RowsPhotoAlbum,
 } from "react-photo-album";
 import "react-photo-album/rows.css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 const renderImage = (
   { alt = "", title, sizes }: RenderImageProps,
   { photo, width, height }: RenderImageContext,
 ) => (
   <div
-    className="relative w-full"
+    className="hover:drop-shadow-glow relative w-full cursor-pointer transition duration-200"
     style={{ aspectRatio: `${width} / ${height}` }}
   >
     <Image
@@ -28,8 +31,24 @@ const renderImage = (
   </div>
 );
 
-const PhotoGallery = () => (
-  <RowsPhotoAlbum photos={photos} render={{ image: renderImage }} />
-);
+const PhotoGallery = () => {
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <RowsPhotoAlbum
+        photos={photos}
+        render={{ image: renderImage }}
+        onClick={({ index: current }) => setIndex(current)}
+      />
+      <Lightbox
+        index={index}
+        slides={photos}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
+    </>
+  );
+};
 
 export default PhotoGallery;
