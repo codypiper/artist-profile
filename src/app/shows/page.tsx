8bd96451c32c923df7df99f216e712a982b27fb9
Cody Piper <piperccody@gmail.com>
2025-06-11 14:35:43 -0400

    Update Shows page to revalidate

--- src/app/shows/page.tsx
+++ src/app/shows/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   title: "Shows - Cody Piper",
 };
 
+export const revalidate = 3600;
+
 const Shows = async () => {
   const shows = await database
     .selectFrom("shows")
@@ -18,11 +20,11 @@ const Shows = async () => {
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center px-4 py-40">
-      <h1 className="mb-12 text-center text-3xl font-bold drop-shadow-dark">
+      <h1 className="drop-shadow-dark mb-12 text-center text-3xl font-bold">
         SHOWS
       </h1>
       {shows.length === 0 ? (
-        <p className="italic drop-shadow-dark">new show dates TBD</p>
+        <p className="drop-shadow-dark italic">new show dates TBD</p>
       ) : (
         <>
           <ol className="flex w-full max-w-4xl flex-col">
@@ -30,7 +32,7 @@ const Shows = async () => {
               <ShowListing key={show.start_date.toISOString()} {...show} />
             ))}
           </ol>
-          <p className="opacity-dim mt-12 text-xs italic drop-shadow-dark">
+          <p className="opacity-dim drop-shadow-dark mt-12 text-xs italic">
             * times are displayed using the local timezone of the venue
           </p>
         </>
