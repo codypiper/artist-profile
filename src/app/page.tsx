38c37a27e71752b14bc1fee28a9ac76c54c9f273
Cody Piper <piperccody@gmail.com>
2025-06-18 15:47:27 -0400

    Update home page and remove news listings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import BackgroundImage from "@/components/BackgroundImage";
 import Link from "@/components/Link";
-import YouTubePlayer from "@/components/YouTubePlayer";
 import {
   faFacebook,
   faInstagram,
@@ -13,47 +12,28 @@ import candle from "@pub/images/backgrounds/candle.jpg";
 import odeToALostLove from "@pub/images/records/ode-to-a-lost-love.jpg";
 import Image from "next/image";
 import IconLink from "./components/IconLink";
-import NewsListing from "./components/NewsListing";
 
 const Home = () => (
-  <main className="relative flex min-h-screen flex-col items-center justify-center py-40">
-    <h1 className="drop-shadow-dark mb-10 text-center text-4xl font-bold">
-      NEWS
+  <main className="relative flex min-h-screen flex-col items-center justify-center gap-10 py-40">
+    <h1 className="drop-shadow-dark text-center text-4xl font-bold text-balance">
+      {'"ODE TO A LOST LOVE" OUT NOW'}
     </h1>
-    <ol className="flex w-full max-w-4xl flex-col">
-      <NewsListing heading={`NEW SINGLE OUT NOW — "Ode to a Lost Love"`}>
-        <Link href="https://music.codypipermedia.com/ode-to-a-lost-love">
-          <Image
-            className="w-full max-w-64"
-            src={odeToALostLove}
-            alt={`"Ode to a Lost Love" artwork`}
-            priority
-            height={250}
-            width={250}
-          />
-          <p className="opacity-dim drop-shadow-dark mt-1 text-center text-xs">
-            click to listen
-          </p>
-        </Link>
-      </NewsListing>
-      <NewsListing heading={`"Be Quiet" music video:`}>
-        <div className="mb-2 w-200 max-w-screen shadow-2xl shadow-black">
-          <YouTubePlayer
-            videoId="DcRw1sY3QUA"
-            title="Be Quiet (Official Video)"
-          />
-        </div>
-      </NewsListing>
-      <NewsListing heading={`"Be Quiet" recorded live in Nashville:`}>
-        <div className="mb-2 w-200 max-w-screen shadow-2xl shadow-black">
-          <YouTubePlayer
-            videoId="todbEDa2X3s"
-            title="Be Quiet (Live Solo Performance)"
-          />
-        </div>
-      </NewsListing>
-    </ol>
-    <ul className="mt-10 flex flex-row items-center gap-4">
+    <div className="border-white/dim max-w-[70vw] border-b pb-10">
+      <Link href="https://music.codypipermedia.com/ode-to-a-lost-love">
+        <Image
+          className="w-full"
+          src={odeToALostLove}
+          alt={`"Ode to a Lost Love" artwork`}
+          priority
+          height={400}
+          width={400}
+        />
+        <p className="opacity-dim drop-shadow-dark mt-1 text-center text-sm">
+          click to listen
+        </p>
+      </Link>
+    </div>
+    <ul className="border-white/dim flex flex-row items-center gap-4">
       <li>
         <IconLink
           href="https://open.spotify.com/artist/4ajrDORPXqqfy4Ce8NHSoO"
