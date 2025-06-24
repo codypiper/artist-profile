bfdf5f1985794a875b1cf6288d8851cc4f0d215c
Cody Piper <piperccody@gmail.com>
2025-06-24 13:04:47 -0400

    Clean up and improve accessibility of Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 interface ButtonProps {
   children: React.ReactNode;
   type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
@@ -13,22 +15,29 @@ const Button: React.FC<ButtonProps> = ({
   isSubmitting = false,
   noShadow = false,
 }) => {
-  disabled = disabled || isSubmitting;
-  noShadow = noShadow || disabled;
+  const isDisabled = disabled || isSubmitting;
+  const hasShadow = !(noShadow || disabled);
 
   return (
     <button
       type={type}
-      disabled={disabled}
-      className={`relative flex h-fit w-fit shrink-0 grow-0 items-center justify-center gap-2 rounded-full border border-white px-4.5 py-2 transition duration-200 ${
-        noShadow ? "" : "drop-shadow-dark hover:drop-shadow-glow"
-      } ${disabled ? "opacity-dim cursor-not-allowed" : "cursor-pointer"}`}
+      disabled={isDisabled}
+      aria-disabled={isDisabled}
+      aria-busy={isSubmitting}
+      className={clsx(
+        "relative flex h-fit w-fit shrink-0 grow-0 items-center justify-center gap-2 rounded-full border border-white px-4.5 py-2 transition duration-200",
+        isDisabled ? "opacity-dim cursor-not-allowed" : "cursor-pointer",
+        hasShadow && "drop-shadow-dark hover:drop-shadow-glow",
+      )}
     >
-      <span className={`${isSubmitting ? "invisible" : ""}`}>{children}</span>
+      <span className={clsx(isSubmitting && "invisible")}>{children}</span>
 
       {/* Spinner while loading */}
       {isSubmitting && (
-        <div className="absolute h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent" />
+        <>
+          <div className="absolute h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent" />
+          <span className="sr-only">Submitting...</span>
+        </>
       )}
     </button>
   );
