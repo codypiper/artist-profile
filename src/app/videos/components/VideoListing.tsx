f2d31f53294849c80f47138ade944d4785695561
Cody Piper <piperccody@gmail.com>
2025-07-01 16:21:52 -0400

    Fix line underneath VideoListing component on mobile screens

--- src/app/videos/components/VideoListing.tsx
+++ src/app/videos/components/VideoListing.tsx
@@ -4,7 +4,7 @@ import type { Video } from "@/lib/database";
 type VideoListingProps = Omit<Video, "rank">;
 
 const VideoListing: React.FC<VideoListingProps> = ({ id, title }) => (
-  <li className="after:border-white/dim mx-6 flex flex-col items-center gap-3 pt-10 not-last:after:mt-7 not-last:after:w-96 not-last:after:border-b">
+  <li className="after:border-white/dim mx-6 flex flex-col items-center gap-3 pt-10 not-last:after:mt-7 not-last:after:w-96 not-last:after:max-w-full not-last:after:border-b">
     <h2 className="drop-shadow-dark text-center text-2xl font-semibold">
       {title}
     </h2>
