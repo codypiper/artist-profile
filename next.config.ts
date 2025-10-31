36c9ac48414adf9686d4e9cc4feda39faf11d62b
Cody Piper <piperccody@gmail.com>
2025-10-30 23:21:59 -0400

    Add /shop redirect

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/shop",
+        destination: "https://shop.codypipermedia.com",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
